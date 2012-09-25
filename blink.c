340c379c950a078b20925cab7e5e90a3572a2e3c
Ganesh <ganeshredcobra@gmail.com>
2012-09-26 01:29:21 +0530

    Hello World example

--- blink.c
+++ blink.c
@@ -5,19 +5,19 @@ void main()
 	int x;
 	while(True)
 	{
-	x=input(pin_B1);
-	if(x)
-		{
-			output_low(pin_B2);
-			output_high(pin_B0);
-			delay_us(50);
-			output_low(pin_B0);
-			delay_us(50);
-		}
-		else
-		{
-			output_high(pin_B2);
-		}		
-}	
+		x=input(pin_B1);
+		if(x)
+			{
+				output_low(pin_B2);
+				output_high(pin_B0);
+				delay_us(50);
+				output_low(pin_B0);
+				delay_us(50);
+			}
+			else
+			{
+				output_high(pin_B2);
+			}		
+	}	
 
 }
